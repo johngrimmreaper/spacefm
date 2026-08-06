1cc554fe59ea531fa18a80554c3980dca0cd7bcc
Reaper <JohnGrimmReaper@disroot.org>
2026-08-06 22:58:09 +0000

    build: check for libtoolize during bootstrap

--- autogen.sh
+++ autogen.sh
@@ -42,9 +42,9 @@ DIE=0
 }
 
 (grep "^AM_PROG_LIBTOOL" $srcdir/$configure_ac >/dev/null) && {
-  (libtool --version) < /dev/null > /dev/null 2>&1 || {
+  (libtoolize --version) < /dev/null > /dev/null 2>&1 || {
     echo
-    echo "**Error**: You must have \`libtool' installed."
+    echo "**Error**: You must have \`libtoolize' installed."
     echo "You can get it from: ftp://ftp.gnu.org/pub/gnu/"
     DIE=1
   }
